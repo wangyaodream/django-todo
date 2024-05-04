a64621e979e1c3a524bef98642ce6d7d7b1eafdc
wangyao <wangyao.dream@hotmail.com>
2024-05-04 17:26:10 +0800

    add CustomModal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,7 +65,66 @@ class App extends Component {
   };
 
   renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+        (item) => item.completed === viewCompleted
+    );
+    return newItems.map((item) => (
+        <li
+            key={item.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span
+              className={`todo-title mr-2 ${
+                this.state.viewCompleted ? "completed-todo" : ""
+              }`}
+              title={item.description}
+          >
+            {item.title}
+          </span>
+          <span>
+            <button
+                className="btn btn-secondary mr-2"
+            >
+              Edit
+            </button>
+            <button
+                className="btn btn-danger"
+            >
+             Delete
+            </button>
+          </span>
 
+        </li>
+    ));
   };
 
+  render() {
+    return (
+        <main className="container">
+          <h1 className="text-black-50 text-uppercase text-center my-4">Todo app</h1>
+          <div className="row">
+            <div className="col-md-6 col-sm-19 mx-auto p-0">
+              <div className="card p-3">
+                <button
+                    className="btn btn-primary"
+                >
+                  Add task
+                </button>
+
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+
+            </div>
+          </div>
+
+        </main>
+    )
+  }
+
 }
+
+export default App;
