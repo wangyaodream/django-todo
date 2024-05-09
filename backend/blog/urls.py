6f5cad9fbe941c9b7ad322b3a912f68533179b7c
wangyao <wangyao.dream@hotmail.com>
2024-05-09 22:50:19 +0800

    use mixins and generic

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,8 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    re_path(r'^articles/$', views.article_list),
-    re_path(r'^articles/(?P<pk>[0-9]+)$', views.article_detail),
+    # re_path(r'^articles/$', views.article_list),
+    # re_path(r'^articles/(?P<pk>[0-9]+)$', views.article_detail),
+    re_path(r'^articles/$', views.ArticleList.as_view(), name='article-list'),
+    re_path(r'^articles/(?P<pk>\d+)$', views.ArticleDetail.as_view(), name='article-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
